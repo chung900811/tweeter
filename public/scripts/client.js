5721af1296281ff6ecf00be127dbeeb86790366b
terry <fc.ho@hotmail.com>
2022-05-29 08:58:29 +0000

    repsonsive design

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,75 +1,93 @@
 $(document).ready(function () {
-    //console log appears on the console (dev tools) on page.  
-    console.log("ready!");
+  //console log appears on the console (dev tools) on page.  
+  console.log("ready!");
 
-    const loadTweets = function() {
-        $.ajax('/tweets', { method: 'GET' })
-        .then(function (moreTweets) {
-          console.log('Success: ', moreTweets);
-          renderTweets(moreTweets);    
-    
-        });   
-      }     
-         loadTweets();
-         
-    $("#new-tweet").on('submit', function(event){
-
-        event.preventDefault();
-        
-        //capturing the value of the form
-    const tweetStr = $("#tweet-input").val();
+  const loadTweets = function() {
+      $.ajax('/tweets', { method: 'GET' })
+      .then(function (moreTweets) {
+        console.log('Success: ', moreTweets);
+        renderTweets(moreTweets);    
+  
+      });   
+    }     
+       loadTweets();
        
-        if (!tweetStr) {
-            $('.error-section').slideDown('fast');
-            $('#error-message').text('Please enter text');
-            return;
-            }
-        if (tweetStr.length > 140) {
-            $('.error-section').slideDown('fast');
-            $('#error-message').text('Max characters exceeded');
-            return;
-            }
-    const serializedData = $(this).serialize();
+  $("#new-tweet").on('submit', function(event){
 
-        $.post('/tweets/', serializedData, (response) => {
-            $('.error-section').slideUp('fast');
-            $('#tweet-input').val('');
-            $('.counter').val('140');
-            console.log(response);
-            loadTweets();
-    });      
-})
-
+      event.preventDefault();
       
-    const renderTweets = function (tweets) {
-        for (const tweet of tweets) {
-        createTweetElement(tweet);
-        }
-        
-    }
+      //capturing the value of the form
+  const tweetStr = $("#tweet-text").val();
+     
+      if (!tweetStr) {
+          $('.error-section').slideDown('fast');
+          $('#error-message').text('Please enter text');
+          return;
+          }
+      if (tweetStr.length > 140) {
+          $('.error-section').slideDown('fast');
+          $('#error-message').text('Max characters exceeded');
+          return;
+          }
+  const serializedData = $(this).serialize();
+
+      $.post('/tweets/', serializedData, (response) => {
+          $('.error-section').slideUp('fast');
+          $('#tweet-text').val('');
+          $('.counter').val('140');
+          console.log(response);
+          loadTweets();
+  });      
 })
-    const createTweetElement = function(tweet) {
-        let $tweet = $(
-            `
-        <div class="maintweetcontent">
 
-        <div class="user-logo">
-        <div>
-        <img src="${tweet.user.avatars}" style="width:28px; height:28px;">
-        <label>${tweet.user.name}</label>
-        </div> 
-        <div> 
-        <output name="userhandle" class="userhandle" for="tweet-body">${tweet.user.handle}</output>
-        </div> 
-        </div>
-        <p class = "maincontent">${tweet["content"]["text"]}</p>
-        <div class="content-footer">
-        <span>${timeago.format(tweet.created_at)}</span>
-        <div class="media-icon"><i class="fa-solid fa-flag"></i><i class="fa-solid fa-repeat"></i> </i><i class="fa-solid fa-heart"></i></div>
-        </div>
-        </div>
+    
+  const renderTweets = function (tweets) {
+      for (const tweet of tweets) {
+      createTweetElement(tweet);
+      }
+      
+  }
+})
+  const createTweetElement = function(tweet) {
+      let $tweet = $(
+          `
+          <section class = tweet-container>
+          <article class="article-class">
+            <header>
+              <div class="avatar-pic">
+                <img src=${tweet.user.avatars}>
+                <span>${tweet.user.name}</span>
+              </div>
+              <div class="handle">
+                <p>${tweet.user.handle}</p>
+              </div>
+            </header>
+  
+          <div class="post-tweet">
+            <p>${(escape(tweet.content.text)).replace(/%20/g, " ").replace("%3F", "?").replace("%21", "!").replace(/%2C/g,",")}</p>
+            <hr>
+          </div>
+  
+          <footer>
+            <div class="postedday">
+              <p>${timeago.format(tweet.created_at)}</p>
+            </div>
+            <div class="flag-retweet-like">
+              <span class="icon flag">
+                <i class="fas fa-flag"></i>
+              </span>
+              <span class="icon retweet">
+                <i class="fas fa-retweet"></i>
+              </span>
+              <span class="icon like">
+                <i class="fas fa-heart"></i>
+              </span>
+            </div>
+            </footer>
+          </article>
+        </section>
         `
-    );
-            $("#maintweetcontent").prepend($tweet)   
-}
+  );
 
+         $(".maintweetcontent").prepend($tweet)  
+}
